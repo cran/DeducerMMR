16a20147eeaa5ddc849b5fb2d889c0fcaedfd51c
Alberto Mirisola <alberto.mirisola@gmail.com>
2011-01-28 00:00:00 +0000

    version 0.0-3

--- R/esp.R
+++ R/esp.R
@@ -12,7 +12,7 @@ if(mod1$regr.order>1){dd=mod1$StepI}
 cc<-dd$terms; first<-attr(cc,"term.labels")
 
 
-options(warn=-1);ff<-first != code; options(warn=0);num<-first[ff]
+num<-setdiff(first,code)
 
 #print("Centered Variables Are")
 #print (num)
